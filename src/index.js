6483d770ff94b3fea27854ec53b5320c22e7e192
Андрей Ноготков <borshblack@gmail.com>
2023-07-19 20:14:49 +0300

    Babel cli, использование плагина @babel/plugin-transform-arrow-functions, пресета @babel/preset-env

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// const babel = require("@babel/core");
+// const result = babel.transformSync(
+//   "const sampleFunction = (value) => {return value + value}",
+//   { browserslistConfigFile: false }
+// );
+// console.log(result.code);
+
+const sampleFunction = (value) => {
+  return value + value;
+};
